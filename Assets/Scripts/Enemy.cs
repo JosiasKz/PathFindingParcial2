31649c2b0087c1f23f6c44be9e9a4c2620a578ad
Juaznd <juanguerreno1992@gmail.com>
2025-11-23 18:39:48 -0300

    ajustes

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField]List<Node> _patrolNodes = new List<Node>();
+    [SerializeField] List<Node> _pathfindingNodes = new List<Node>();
     [SerializeField] float _speed;
     [SerializeField]LayerMask wallLayer;
     [SerializeField] float _searchRadius;
+    bool pathFinding = false;
     Node _currentNode;
 
     //public void SetPos(Vector3 pos)
@@ -25,11 +27,19 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (_patrolNodes.Count > 0)
         {
             if (!LineOfSight(_patrolNodes[0].transform))
             {
+                Debug.Log("buscando otro nodo cercano");
                 _currentNode = getClosestNode(_searchRadius);
+            }
+            else
+            {
+                Debug.Log("Nos quedamos con el primer nodo");
+
+                _currentNode = _patrolNodes[0];
             }
         }
 
@@ -39,7 +49,7 @@ public class Enemy : MonoBehaviour
             //dir.z = 0;
             transform.position += dir.normalized * Time.deltaTime * _speed;
 
-            if (dir.magnitude < 0.1f)
+            if (dir.magnitude < 0.2f)
             {
                 _currentNode=null;
             }
@@ -79,7 +89,7 @@ public class Enemy : MonoBehaviour
         {
             Node node = obj.GetComponent<Node>();
             if (node==null) continue;
-
+            if (!LineOfSight(obj.transform)) continue;
             float dist = Vector3.Distance(transform.position, node.transform.position);
 
             if (dist < minDist)
